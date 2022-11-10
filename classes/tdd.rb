d06319def49ccb0be87133964b9380cbb06aa354
Amtresu <conor.follette@gmail.com>
2022-11-10 17:15:14 +0900

    added tests for class methods

---
+++ classes/tdd.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# classes/tdd.rb
+class Tests
+  def reverse_string(str)
+    str.reverse
+  end
+
+  def factorial(number)
+    result = number
+    while number > 1
+      result *= (number - 1)
+      number -= 1
+    end
+    result
+  end
+
+  def fizz_buzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'FizzBuzz'
+    elsif (num % 3).zero?
+      'Fizz'
+    elsif (num % 5).zero?
+      'Buzz'
+    else
+      num
+    end
+  end
+end
